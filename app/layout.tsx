585c0f4aaadbf95fa93775cc467ae1c5aef67747
rossvasquez <ross.vasquez.17@gmail.com>
2023-10-27 17:03:09 -0500

    Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,23 +8,22 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <>
-    <Head>
-      <title>Ross Vasquez Portfolio</title>
-      <meta name="description" content="A collection of work." key="desc" />
-      <meta property="og:title" content="Ross' Portfolio" />
-      <meta
-        property="og:description"
-        content="A collection of work."
-      />
-      <meta
-        property="og:image"
-        content="https://evoumvdxqcveaclexrxv.supabase.co/storage/v1/object/public/Images/this_site.png"
-      />
-    </Head>
     <html lang="en">
+      <Head>
+        <title>Ross Vasquez Portfolio</title>
+        <meta name="description" content="A collection of work." key="desc" />
+        <meta property="og:title" content="Ross' Portfolio" />
+        <meta
+          property="og:description"
+          content="A collection of work."
+        />
+        <meta
+          property="og:image"
+          content="https://evoumvdxqcveaclexrxv.supabase.co/storage/v1/object/public/Images/this_site.png"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>{children}</body>
     </html>
-    </>
   )
 }
