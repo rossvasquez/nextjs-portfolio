232febf48fe1dd8fe3d02a29ac615c7e421ae6b6
rossvasquez <ross.vasquez.17@gmail.com>
2023-10-27 16:48:06 -0500

    Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,20 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Playfair_Display } from 'next/font/google'
 
+import Head from 'next/head'
+
 const inter = Inter({ subsets: ['latin'] })
 const playfair = Playfair_Display({ subsets: ['latin'], weight: '600' })
 
 export const metadata: Metadata = {
   title: 'Ross Vasquez',
   description: 'A portfolio of technological literacy.',
+  openGraph: {
+    title: "Ross Vasquez",
+    description: "A collection of work.",
+    url: "https://rossvasquez.me",
+    images: "https://evoumvdxqcveaclexrxv.supabase.co/storage/v1/object/public/Images/this_site.png"
+  }
 }
 
 export default function RootLayout({
