cb79d6a7374a6e25cd349f195197635f166ca5c8
rossvasquez <ross.vasquez.17@gmail.com>
2023-10-27 16:56:15 -0500

    Changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,6 @@
 import './globals.css'
-import type { Metadata } from 'next'
-
-import { Inter } from 'next/font/google'
-import { Playfair_Display } from 'next/font/google'
 
 import Head from 'next/head'
-
-const inter = Inter({ subsets: ['latin'] })
-const playfair = Playfair_Display({ subsets: ['latin'], weight: '600' })
-
-export const metadata: Metadata = {
-  title: 'Ross Vasquez',
-  description: 'A portfolio of technological literacy.',
-  openGraph: {
-    title: "Ross Vasquez",
-    description: "A collection of work.",
-    url: "https://rossvasquez.me",
-    images: "https://evoumvdxqcveaclexrxv.supabase.co/storage/v1/object/public/Images/this_site.png"
-  }
-}
 
 export default function RootLayout({
   children,
@@ -26,8 +8,23 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    <>
+    <Head>
+      <title>Ross Vasquez Portfolio</title>
+      <meta name="description" content="A collection of work." key="desc" />
+      <meta property="og:title" content="Ross' Portfolio" />
+      <meta
+        property="og:description"
+        content="A collection of work."
+      />
+      <meta
+        property="og:image"
+        content="https://evoumvdxqcveaclexrxv.supabase.co/storage/v1/object/public/Images/this_site.png"
+      />
+    </Head>
     <html lang="en">
       <body>{children}</body>
     </html>
+    </>
   )
 }
